69a417fa73b4fcfede9d3df8326b2474b0804490
Theresa <ths.stadler@gmail.com>
2020-10-16 18:04:32 +0200

    Add LR loss

--- projects/project1/scripts/costs.py
+++ projects/project1/scripts/costs.py
@@ -18,6 +18,13 @@ def compute_gradient_mse(y, tx, w):
     return -tx.T.dot(e) / len(e)
 
 
+def compute_loss_ridge(y, tx, w, lambda_):
+    loss = compute_loss_mse(y, tx, w)
+    penal_loss = loss + lambda_ * w.dot(w)
+
+    return penal_loss
+
+
 def compute_loss_lasso(y, tx, w, lambda_):
     """Calculate the Lasso loss under weights vector w."""
     e = y - tx.dot(w)
@@ -31,3 +38,54 @@ def compute_gradient_lasso(y, tx, w, lambda_):
     subgrad = lambda_ * np.sign(w)
 
     return -tx.T.dot(e)/len(e) + subgrad
+
+
+def compute_loss_logreg(y, tx, w):
+    """Compute the loss under a logistic regression model (negative log likelihood) with class labels {0, 1}."""
+    assert len(set(y).difference({0., 1.})) == 0, "Class labels must be encoded as {0, 1}"
+
+    a = np.log(1 + np.exp(tx.dot(w)))
+
+    b = -y * tx.dot(w)
+
+    return a.dot(b)
+
+
+def compute_gradient_logreg(y, tx, w):
+    """Compute the gradient of the negative log-likelihood under a logistic regression model  with class labels {0, 1}."""
+    assert len(set(y).difference({0., 1.})) == 0, "Class labels must be encoded as {0, 1}"
+
+    s = sigmoid(tx.dot(w)) - y
+    grad = tx.T.dot(s)
+
+    return grad
+
+
+def compute_hessian_logreg(tx, w):
+    """Compute the Hessian of the negative log-likelihood under a logistic regression model."""
+    t = tx.dot(w)
+    s = np.diag(sigmoid(t)*(1 - sigmoid(t)))
+
+    return tx.T.dot(s).dot(tx)
+
+
+def compute_loss_logreg_reg(y, tx, w, lambda_):
+    """Compute the loss of the negative log-likelihood under a logistics regression model under L2 regularisation"""
+    loss = compute_loss_logreg(y, tx, w)
+    penal_loss = loss + lambda_ / 2 * np.linalg.norm(w)
+
+    return penal_loss
+
+
+def compute_gradient_logreg_reg(y, tx, w, lambda_):
+    """Compute the gradient of the negative log-likelihood under a logistics regression model under L2 regularisation"""
+    grad = compute_gradient_logreg(y, tx, w)
+    penal_grad = grad + lambda_ * w
+
+    return penal_grad
+
+
+def sigmoid(t):
+    """apply the sigmoid function on t."""
+
+    return np.exp(t)/(1 + np.exp(t))
